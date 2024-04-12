c30af1390efb3e4f4d235dbb204acee012bd7600
kaisbad <kaiz8@nycstudents.net>
2024-04-12 12:23:55 -0400

    a

---
+++ app1.py
@@ -0,0 +1,9 @@
+import json
+## Open the JSON file of movie data
+movies = open("./movies.json", encoding="utf8")
+## create variable "data" that represents the enitre movie list
+data = json.load(movies)
+
+
+
+
