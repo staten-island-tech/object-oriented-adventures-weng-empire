72d8c45e14726309ffbc53adadf35d8a0de00750
WWeimingW <weimingw@nycstudents.net>
2024-04-17 11:45:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,21 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+class Character():
+    def __init__(self, name):
+        self.name = name
+        self.maxhealth = 100
+        self.attack = 10
+        self.gold = 0
+        self.weapon = ["Basic Sword"]
+
+class Goblin:
+    def __int__(self, name):
+        self.name = name
+        self.maxhealth = 80
+        self.attack = 7
+        self.goldgain = 20
+
+class Slime
+        
+        
