b45e6672ae8707588c287d8d68087614fd36b25a
kaisbad <kaiz8@nycstudents.net>
2024-05-06 11:41:43 -0400

    a

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import json
-import random 
-import os
-import sys
+import json, random, os, sys
 ## Open the JSON file of movie data
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
@@ -21,6 +18,7 @@ class Goblin():
     def __init__(self):
         self.name = ["Goblin"]
         self.maxhealth = 80
+        self.health = self.maxhealth
         self.attack = 7
         self.goldgain = random.randint(5,15)
 
@@ -28,6 +26,7 @@ class Slime():
     def __init__(self):
         self.name = ["Slime"]
         self.maxhealth = 50
+        self.health = self.maxhealth
         self.attack = 5
         self.goldgain = random.randint(1,10)
 
@@ -35,6 +34,7 @@ class Knight():
     def __init__(self):
         self.name = ["Knight"]
         self.maxhealth = 140
+        self.health = self.maxhealth
         self.attack = 15
         self. goldgain = random.randint(25,50)
         self.weapon = ["Sword"]
@@ -42,16 +42,18 @@ class Knight():
 class Zombie():
     def __init__(self, name):
         self.name = name
-        self.maxhealth = 120
-        self.attack = 18
-        self.goldgain = 35
+        self.maxhealth = 100
+        self.health = self.maxhealth
+        self.attack = 12
+        self.goldgain = random.randint(10,25)
 
 class Mark():
     def __init__(self, name):
         self.name = name
         self.maxhealth = 200000
-        self.attack = 1
-        self.goldgain = 0.0000000000000000000000000001
+        self.health = self.maxhealth
+        self.attack = 0.05
+        self.goldgain = 0.000000000000001
 
 class Main():
     def main():
@@ -60,9 +62,162 @@ class Main():
         print("2.) Exit")
         option = input("-> ")
         if option == "1":
-            start()
+            St.start()
         elif option == "2":
             sys.exit()
         else:
             print('Please pick a valid option')
-            Main.main()
+            Ma.main()
+
+class Start():
+    def start():
+        print("Hello, what is your name?")
+        option = input("--> ")
+        global player 
+        player = Character(option)
+        Mn.menu
+
+class Menu():
+    def menu():
+        print("Name: %s") % player.name
+        print("Attack: %i") % player.attack
+        print("Gold: %i") % player.gold
+        print("Current Weapons: %s") % player.weapon
+        print("Potions: %i") % player.potions
+        print("Health: %i/%i\n") % (player.health, player.maxhealth)
+        print("1.) Fight")
+        print("2.) Store")
+        print("3.) Inventory")
+        print("4.) Exit")
+        option = input("--> ")
+        if option == "1":
+            Ep.enemypick()
+        elif option == "2":
+            St.store()
+        elif option == "3":
+            I.inventory()
+        elif option == "4":
+            sys.exit()
+        else:
+            print("Please pick a valid option")
+            Mn.menu()
+
+def attack(self):
+    attack = self.attack
+    if self.weapon == "Basic Sword":
+        attack += 5
+    elif self.weapon == "Great Sword":
+        attack += 25
+    elif self.weapon == "Dragon Slayer":
+        attack += 50
+    return attack
+
+class Inventory():
+    def inventory():
+        print("What do you want to do?")
+        print("1.) Equip Weapon")
+        print("2.) Go back")
+
+class Equip():
+    def equip():
+        print("What do you want to equip?")
+
+class Enemypick():
+    def enemypick():
+        global enemy
+        enemynum = random.randint(1, 4)
+        if enemynum == 1:
+            enemy = Goblin
+        elif enemynum == 2:
+            enemy = Knight
+        elif enemynum == 3:
+            enemy = Slime
+        else:
+            enemy = Zombie
+        F.fight()
+
+class Fight():
+    def fight():
+        print()
+    fight()
+
+def attackdamage():
+    attackdamage()
+
+class Potionfight():
+    def potionfight():
+        if player.potions == 0:
+            print("You don't have any potions")
+        else:
+            player.health += 50
+            if player.health > player.maxhealth:
+                player.health = player.maxhealth
+            print("You drank a potion!")
+        option = input(' ')
+        F.fight()
+
+class Potionmenu():
+    def potionmenu():
+        if player.potions == 0:
+            print("You don't have any potions")
+        else:
+            player.health += 50
+            if player.health > player.maxhealth:
+                player.health = player.maxhealth
+            print("You drank a potion!")
+        option = input(' ')
+        Mn.menu()
+
+class Run():
+    def run():
+        runchance = random.randint(1, 2)
+        if runchance == '1':
+            print("You successfully ran away!")
+            option = input(' ')
+            Mn.menu()
+        else:
+            print("You failed to get away!")
+            option = input(' ')
+
+class Win():
+    def win():
+        player.gold += enemy.goldgain
+        print("You have defeated the %s") % enemy.name
+        print("You found %i gold!") % enemy.goldgain
+        option = input(' ')
+        Mn.menu()
+
+class Dead():
+    def dead():
+        print("You have died")
+        print("1.) Restart")
+        print("2.) Exit")
+        option = input(' ')
+        if option == '1':
+            St.start()
+        elif option == '2':
+            sys.exit
+
+
+weapons = {"Great Sword"}
+class Store():
+    def store():
+        print("Welcome to the shop!")
+        print("\nWhat would you like to buy?\n")
+        print("1.) Great Sword")
+        print("2.) Dragon Slayer")
+        print("3.) Back")
+        option = input(' ')
+
+Ma = Main()
+St = Start()
+Mn = Menu()
+I = Inventory()
+Eq = Equip()
+Ep = Enemypick()
+F = Fight()
+R = Run()
+W = Win()
+D = Dead()
+So = Store()
+Main.main()
