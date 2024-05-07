a833581aac579d78cd09eb138aa02a2d7aaf4765
WWeimingW <weimingw@nycstudents.net>
2024-05-07 12:10:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,48 +13,17 @@ class Character():
         self.gold = 0
         self.potions = 0
         self.weapon = ["Basic Sword"]
-
-class Goblin():
-    def __init__(self):
-        self.name = ["Goblin"]
-        self.maxhealth = 80
-        self.health = self.maxhealth
-        self.attack = 7
-        self.goldgain = random.randint(5,15)
-
-class Slime():
-    def __init__(self):
-        self.name = ["Slime"]
+class Enemy():
+    def __init__(self, name):
+        self.name = name
         self.maxhealth = 50
         self.health = self.maxhealth
         self.attack = 5
-        self.goldgain = random.randint(1,10)
-
-class Knight():
-    def __init__(self):
-        self.name = ["Knight"]
-        self.maxhealth = 140
-        self.health = self.maxhealth
-        self.attack = 15
-        self. goldgain = random.randint(25,50)
-        self.weapon = ["Sword"]
-
-class Zombie():
-    def __init__(self, name):
-        self.name = name
-        self.maxhealth = 100
-        self.health = self.maxhealth
-        self.attack = 12
-        self.goldgain = random.randint(10,25)
-
-class Mark():
-    def __init__(self, name):
-        self.name = name
-        self.maxhealth = 200000
-        self.health = self.maxhealth
-        self.attack = 0.05
-        self.goldgain = 0.000000000000001
-
+        self.goldgain = random.randint(5,15)
+Goblin = Enemy()
+Knight = Enemy()
+Slime = Enemy()
+Zombie = Enemy()
 class Main():
     def main():
         print("Welcome!\n")
@@ -182,8 +151,8 @@ class Run():
 class Win():
     def win():
         player.gold += enemy.goldgain
-        print("You have defeated the %s") % enemy.name
-        print("You found %i gold!") % enemy.goldgain
+        print(f"You have defeated {enemy.name}")
+        print(f"You found %i gold! {enemy.goldgain}")
         option = input(' ')
         Mn.menu()
 
@@ -221,5 +190,3 @@ W = Win()
 D = Dead()
 So = Store()
 Main.main()
-
-ekjhfheirbvburhkfuherg
