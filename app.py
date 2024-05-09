05bf18bb3aaf9c82571045e40b1cbe92b5a2a306
kaisbad <kaiz8@nycstudents.net>
2024-05-09 07:36:23 -0400

    a

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class Player():
         self.maxhealth = 100
         self.health = self.maxhealth
         self.attack = 10
-        self.gold = 0
+        self.gold = 1000
         self.potions = 2
         self.weapon = 'Basic Sword'
 
@@ -106,10 +106,12 @@ def attack(self):
     attack = self.attack
     if self.weapon == "Basic Sword":
         attack += 5
-    elif self.weapon == "Great Sword":
+    elif self.weapon == "Katana":
         attack += 25
     elif self.weapon == "Dragon Slayer":
         attack += 50
+    elif attack.weapon == "AK-47":
+        attack += 100
     return attack
 
 class Inventory():
@@ -199,16 +201,68 @@ class Dead():
             sys.exit
 
 
-weapons = {"Great Sword"}
+
 class Store():
     def store():
-        print("Welcome to the shop!")
-        print("\nWhat would you like to buy?\n")
-        print("1.) Great Sword (50 Gold)")
+        print("Welcome to the Store!")
+        print("\nWhat would you like to purchase? (Enter full name of item)\n")
+        print("1.) Katana (50 Gold)")
         print("2.) Dragon Slayer (200 Gold)")
-        print("3.) Potion (10 Gold)")
-        print("4.) Back")
+        print("3.) AK-47 (500 Gold)")
+        print("4.) Potion (10 Gold)")
+        print("5.) Back")
         option = input(' ')
-
+        if option == "1":
+            weapon = "Katana"
+            if player.gold >= 50:
+                player.gold -= 50
+                player.weapon.append(weapon)
+                print(f"You have bought a Katana")
+                option = input(' ')
+                Store.store()
+            else:
+                print("You don't have enough gold")
+                option = input(' ')
+                Store.store()
+        elif option == "2":
+            weapon = "Dragon Slayer"
+            if player.gold >= 200:
+                player.gold -= 200
+                player.weapon.append(weapon)
+                print(f"You have bought the Dragon Slayer")
+                option = input(' ')
+                Store.store()
+            else:
+                print("You don't have enough gold")
+                option = input(' ')
+                Store.store()
+        elif option == "3":
+            weapon = "AK-47"
+            if player.gold >= 500:
+                player.gold -= 500
+                player.weapon.append(weapon)
+                print(f"You have bought an AK-47")
+                option = input(' ')
+                Store.store()
+            else:
+                print("You don't have enough gold")
+                option = input(' ')
+                Store.store()
+        elif option == "4":
+            if player.gold >= 10:
+                player.gold -= 10
+                player.potions += 1
+                print(f"You have bought a potion")
+                option = input(' ')
+                Store.store()
+            else:
+                print("You don't have enough gold")
+                option = input(' ')
+                Store.store()
+        elif option == "5":
+            Menu.menu()
+        else:
+            print("Please pick a valid option")
+            Store.store()
 
 Main.main()
